6776fa66a4a25b085c3190474bbadc7547809f38
Robby Grossman <robby@freerobby.com>
2014-10-21 10:44:08 -0400

    Unique querying - it works!!

--- proveit.rb
+++ proveit.rb
@@ -4,6 +4,8 @@ require 'riak'
 require 'timerizer'
 require 'json'
 
+require 'time'
+
 NUM_INSERTS = 1000
 
 # Setup
@@ -21,19 +23,22 @@ NUM_INSERTS.times do |t|
   puts "Generating object #{t + 1}" if (t + 1) % 100 == 0
   obj = bucket.get_or_new(t.to_s)
   obj.data = {
+    'account_id_i' => (rand * 10).to_i,
     'type_s' => ['load', 'play'].sample,
-    'uuid_s' => "user-#{(rand * 1000).to_i}",
-    'media_id_i' => (rand * 100).to_i,
-    'created_at_dt' => (rand * 100).to_i.days.ago
+    'uuid_s' => "user-#{(rand * 30).to_i}",
+    'media_id_i' => (rand * 90).to_i,
+    # 'created_at_dt' => (rand * 100).to_i.days.ago.to_time.iso8601
   }
-  obj.store#(type: 'events_index')
+  obj.store
 end
 
-puts "Waiting 3 seconds for indexing"
-sleep 3
+puts "Waiting 1 second for indexing"
+sleep 1
 
 # Search
 
-result = client.search('events', 'uuid_s:load')
-puts result
-puts "Found #{result['num_found']} loads."
+puts "How many unique users loaded a media for account 7? ngroups should be ~22-28."
+puts JSON.parse(`curl "http://localhost:8098/search/query/events_index?wt=json&indent=true&rows=0&q=type_s:load%20AND%20account_id_i:7&group=true&group.field=uuid_s&group.ngroups=true" --silent`)['grouped']['uuid_s']['ngroups']
+
+puts "How many total loads were there for account 7? numFound should be ~50."
+puts JSON.parse(`curl "http://localhost:8098/search/query/events_index?wt=json&indent=true&rows=0&q=type_s:load%20AND%20account_id_i:7" --silent`)['response']['numFound']
